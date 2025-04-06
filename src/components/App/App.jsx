d08b56ef4a693c18932f9e628dd00d95a315685c
Mr.Elijah <beztalannyj.ilia@gmail.com>
2025-04-05 20:40:27 -1000

    selectors

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,20 @@
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import "../../index.css";
 import { useEffect } from "react";
 import { fetchContacts } from "../../redux/contactOps";
+import { selectLoading, selectError } from "../../redux/contactSlice";
 
 export default function App() {
   const dispatch = useDispatch();
+
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -19,6 +25,8 @@ export default function App() {
       <h1 className="title">Phone book</h1>
       <ContactForm />
       <SearchBox />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
       <ContactList />
     </div>
   );
